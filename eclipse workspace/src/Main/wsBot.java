adbe698fb0a773e3a95b354bf8215f0251c39fd6
6god <tdnguy37@asu.edu>
2021-12-05 16:50:10 -0700

    main method

---
+++ eclipse workspace/src/Main/wsBot.java
@@ -0,0 +1,30 @@
+package Main;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class wsBot {
+	
+	public static JDA jda;
+	
+	
+	
+	//begin Main method
+	public static void main(String[] args) throws LoginException {
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
